9cac231af744c04a02c30e3c2dc887b0bd8a6920
BBloke <bbloke@blueyonder.co.uk>
2021-12-03 01:04:33 +0000

    Create if.myArrayCheckbox.php

---
+++ assets/if/custom/if.myArrayCheckbox.php
@@ -0,0 +1,46 @@
+<?php
+// Subject must be a valid document checkbox TV
+// Example:
+// [[if? &is=`checkbox:myArrayCheckbox` &custom=`myArrayCheckbox` &then=`Matched [+myArrayCheckboxResult+] Results` ]] 
+//
+// this is more dynamic and only requires the TV name.  It will get the choosen documnet values and TV options from the TV Name
+// 
+// Returns the number of matches.
+
+function myArrayCheckbox ($subject, $operand) {
+	$modx = EvolutionCMS();
+
+	$i = 0;
+	
+	$data = $modx->getTemplateVarOutput($subject);
+	$data[0] = $data[$subject];
+
+	$tv = $modx->getTemplateVar($subject);
+	$tvValues = $tv['elements'];	
+	$tvResults = explode("||", $tvValues);
+
+	foreach ( $tvResults as $tvVal )
+	{
+		//echo $tvKey. $tvVal;
+		if ( strpos($tvVal, "==") ) 
+		{
+			$val = explode("==", $tvVal);
+			$values[] = $val[1];
+		} else {
+			$values[] = $tvVal;
+		}
+	}
+
+	$output = explode(",", $data[0]);
+	
+	foreach ( $output as $check )
+	{
+	    if ( in_array($check, $values) )
+	    {
+	    	$i++;
+	    }
+	}
+    
+    $modx->setPlaceholder("myArrayCheckboxResult", $i);
+    return $i;
+}
